993f5017fd8d85daac92091878ce88920474ce58
Robin <guptarobin466@gmail.com>
2022-10-11 14:45:55 +0530

    setting starting condition of game

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Elements
+const score0Ele = document.querySelector("#score--0");
+const score1Ele = document.querySelector("#score--1");
+const dice = document.querySelector(".dice");
+
+// starting conditions
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
+dice.classList.add("hidden");
